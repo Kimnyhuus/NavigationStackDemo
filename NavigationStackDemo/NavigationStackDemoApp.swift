d305d7a248b52ab514443700d0fa86b61ae1815c
Kim Nyhuus <Kimnyhuus@gmail.com>
2024-07-02 01:07:26 +0200

    Initial Commit

---
+++ NavigationStackDemo/NavigationStackDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackDemoApp.swift
+//  NavigationStackDemo
+//
+//  Created by Kim Nyhuus Hansen on 02/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStackDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
